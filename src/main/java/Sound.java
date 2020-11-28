283ebb067708da5d4f3f9a5626ad98717fa7da6b
Spartak <bonus2@gmail.com>
2020-11-29 00:34:49 +0300

    1 commit

---
+++ src/main/java/Sound.java
@@ -0,0 +1,13 @@
+public class Sound {
+    {
+        "audio": {
+        "content": "/* Your audio */"
+    },
+        "config": {
+        "enableAutomaticPunctuation": true,
+                "encoding": "LINEAR16",
+                "languageCode": "ru-RU",
+                "model": "default"
+    }
+    }
+}
